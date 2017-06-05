a81aaebf240ce891f7899f9eeb5ec9d4d0cac709
Fraser <frasers@hotmail.co.uk>
2017-06-05 13:26:51 +0000

    Added a method that takes a variable of any type and displays the type of variable

--- Main.scala
+++ Main.scala
@@ -3,9 +3,21 @@
   */
 object Main {
 
+  //Any allows any data type to be passed in
+  def displayVariableType(value : Any) = {
 
+    //.getClassTag displays the type of the data
+    printf(value.getClass().toString() + "\n")
+  }
 
   def main(args : Array[String]) : Unit = {
-
+    //Double
+    displayVariableType(3.14)
+    //String
+    displayVariableType("Text")
+    //Boolean
+    displayVariableType(false)
+    //Integer
+    displayVariableType(3)
   }
 }
